a3e04a063a117dd7eaf01796c0fc2926d133999b
yukki <solid247+git@gmail.com>
2020-09-16 16:29:19 +0900

    first commit

---
+++ addwlanprofile.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set prof="eduroam"
+set xmlfile=".\Wi-Fi-eduroam.xml"
+
+netsh wlan show profiles | find %prof% > nul
+
+rem if exist wlan profile eduroam delete 
+if not ERRORLEVEL 1 (netsh wlan delete profile name=eduroam)
+
+timeout 2
+rem add wlan profile
+netsh wlan add profile filename=%xmlfile%
